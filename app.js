de5d4e39ee7bec67d00bff6a46be1ce936920726
Tomáš Fiala <tomasfiala555@gmail.com>
2019-10-09 20:20:14 +0200

    var -> let

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+let restify = require('restify'); 
+let builder = require('botbuilder'); 
 
 // Setup Restify Server
-var server = restify.createServer();
+let server = restify.createServer();
 server.listen(process.env.PORT || 3000, function() 
 {
    console.log('%s poslouchá na %s', server.name, server.url); 
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector
+let connector = new builder.ChatConnector
 ({ appId: 'd48bda33-b85e-452a-b466-5a8464105729', appPassword: 'QK7xVaEpxcuQcP7WWCCoVmj' }); 
-var bot = new builder.UniversalBot(connector);
+let bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
